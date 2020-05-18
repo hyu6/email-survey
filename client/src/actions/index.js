7e02c4764facf4625d09aaf8d1ab1dda9f04da15
Haoran Yu <haoranyu6@gmail.com>
2020-05-18 06:58:12 -0700

    update fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
+const fetchUser = () => async (dispatch) => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res });
 };
