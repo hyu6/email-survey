030f16f973044cec019f41246ad57e7c350a3723
Haoran Yu <haoranyu6@gmail.com>
2020-05-18 08:35:32 -0700

    create landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Email Survey</h1>Collect feedback from your customers
+    </div>
+  );
+};
+
+export default Landing;
