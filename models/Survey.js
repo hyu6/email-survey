27d94187784d91c63722e953a28a6b6c44f21ddf
Haoran Yu <haoranyu6@gmail.com>
2020-05-23 02:09:58 -0700

    create recipient schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   subject: String,
   body: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   positive: { type: Number, default: 0 },
   negative: { type: Number, default: 0 },
 });
