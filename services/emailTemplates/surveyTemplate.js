d1c461f0ef8a4d6ff6a32116685549656db0c276
Haoran Yu <haoranyu6@gmail.com>
2020-05-24 16:27:47 -0700

    create survey template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,21 @@
 module.exports = (survey) => {
-  return '<div>' + survey.body + '</div>';
+  return `
+    <html>
+
+    <body>
+      <div style="text-align: center;">
+        <h3>Your Feedback</h3>
+        <p>Please make a response:</p>
+        <p>${survey.body}</p>
+        <div>
+          <a href="http://localhost:3000/">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000/">No</a>
+        </div>
+      </div>
+    </body>
+
+    </html>
+  `;
 };
