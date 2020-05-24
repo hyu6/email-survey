90bcb5e300015516ae60bea3c5294b00511e4b2d
Haoran Yu <haoranyu6@gmail.com>
2020-05-24 09:16:28 -0700

    send requests to sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('hyu6.dev@gmail.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -35,6 +36,17 @@ class Mailer extends helper.Mail {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON(),
+    });
+
+    const response = await this.sgApi.API(request);
+    return response;
   }
 }
 
