038038562615c5c645ba65b174d2267839a5e9d1
Haoran Yu <haoranyu6@gmail.com>
2020-05-24 17:53:19 -0700

    redirect users to a thanks page on response

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const keys = require('../../config/keys');
+
 module.exports = (survey) => {
   return `
     <html>
@@ -8,10 +10,10 @@ module.exports = (survey) => {
         <p>Please make a response:</p>
         <p>${survey.body}</p>
         <div>
-          <a href="http://localhost:3000/">Yes</a>
+          <a href="${keys.redirectDomain}/api/surveys/responded">Yes</a>
         </div>
         <div>
-          <a href="http://localhost:3000/">No</a>
+          <a href="${keys.redirectDomain}/api/surveys/responded">No</a>
         </div>
       </div>
     </body>
