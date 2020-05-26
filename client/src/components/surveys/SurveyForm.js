4ba36bbec5bd502aac0f8daeddf1dac6836a646b
Haoran Yu <haoranyu6@gmail.com>
2020-05-25 23:41:23 -0700

    set up SurveyForm

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,11 @@
+// SurveyForm shows a form for users to add input
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class SurveyForm extends Component {
+  render() {
+    return <div>SurveyForm</div>;
+  }
+}
+
+export default reduxForm({ form: 'surveyForm' })(SurveyForm);
